1df983fbe0763c6d72059a2ad276c52bd7e425b1
James Orson <jamesaorson@gmail.com>
2019-10-08 21:40:49 -0700

    Fixed Sass stuff
    Tasha's husband, who loves her a lot, helped her out in making the SASS stuff work so she can use a non-crappy styling system.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './assets/logo.svg';
-import "./App.css";
+import "./styles/App/App.css";
 import Menu from './components/Menu.js';
 import Footer from './components/Footer';
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <App />,
